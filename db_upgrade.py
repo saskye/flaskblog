bfe26f02ba542b675ff8e2b00b92183fefe7bd5a
guosong <guosong@easybroadnet.com>
2017-02-22 11:36:46 +0800

    add upgrage

---
+++ db_upgrade.py
@@ -0,0 +1,6 @@
+#!venv/bin/pytyhon
+from migrate.versioning import api
+from config import SQLALCHEMY_DATABASE_URI
+from config import SQLALCHEMY_MIGRATE_REPO
+api.upgrade(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
+print 'Currrent database version: ' + str(api.db_version(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO))
