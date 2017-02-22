0c458cd0f5d5ee505b9267095620a6611a921948
guosong <guosong@easybroadnet.com>
2017-02-22 17:15:20 +0800

    some changes

--- app/models.py
+++ app/models.py
@@ -8,7 +8,21 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User %r>' %(self.nickname)
+    def is_authenticated(self):
+    	return True
 
+    def is_active(self):
+        return True
+
+    def is_anonynous(self):
+    	return False
+
+    def get_id(self):
+    	try:
+    		return unicode(self.id)
+        except NameError:
+        	return str(self.id)
+  
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key = True)
