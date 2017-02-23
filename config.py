2db7ba22fe6babc269c67c2d9089c2eb81411e74
guosong <guosong@easybroadnet.com>
2017-02-23 18:49:29 +0800

    add oid

--- config.py
+++ config.py
@@ -12,4 +12,5 @@ OPENID_PROVIDERS = [
     { 'name': 'Yahoo', 'url': 'https://me.yahoo.com' },
     { 'name': 'AOL', 'url': 'http://openid.aol.com/<username>' },
     { 'name': 'Flickr', 'url': 'http://www.flickr.com/<username>' },
+    { 'name': 'openid.org.cn', 'url': 'http://<username>.openid.org.cn/' },
     { 'name': 'MyOpenID', 'url': 'https://www.myopenid.com' }]
