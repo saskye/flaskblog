0c12965243b264ea740f1926105e474908713f80
guosong <guosong@easybroadnet.com>
2017-03-01 17:05:55 +0800

    change

--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 from flask import render_template, flash, redirect, session, url_for, request, g
 from flask.ext.login import login_user, logout_user, current_user, login_required
 from app import app, db, lm, oid
-from forms import LoginForm, Editform
+from forms import LoginForm, EditForm
 from models import User
 from datetime import datetime
 
